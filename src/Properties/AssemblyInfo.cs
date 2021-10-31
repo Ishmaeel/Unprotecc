c2fe449c95a8bfd56cce5042b169fe2aebf5968c
Ishmaeel <Ishmaeel@users.noreply.github.com>
2021-10-31 23:07:09 +0300

    Ascend into transcendence.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Unprotecc")]
 [assembly: AssemblyProduct("Unprotecc")]
-[assembly: AssemblyCopyright("Copyright © Ishmaeel 2019")]
-[assembly: AssemblyDescription("Removes sheet protection from XLSX files.")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © Ishmaeel 2019-2021")]
+[assembly: AssemblyDescription("Removes protection from XLSX files.")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 [assembly: ComVisible(false)]
